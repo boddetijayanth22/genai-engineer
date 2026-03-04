9bf357e58df86acfed8ebe04f2094cd087b5ea61
boddetijayanth22 <boddetijayanth@gmail.com>
2026-03-04 11:39:00 +0530

    Day 1: Project Setup and environment configuration

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-print("GenAI Enginneer environment ready")
+def greet(name: str):
+    return f"hello, {name}! Welcome to your GenAI Journey."
+
+if __name__ == "__main__":
+    name = input("Enter your Name: ")
+    print(greet(name))
